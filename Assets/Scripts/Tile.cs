3e0f0ffa2b74ec7b76dbdb05987be76fea8c5459
justgaret23 <sheehan.garrett23@gmail.com>
2021-07-21 14:46:23 -0500

    Starter code and grid

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool isTraversable;
+    bool isOccupied;
+    Piece pieceOnTile;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
