a89df29d637e0ae30ec5b6c3317b0fd359a903c0
justgaret23 <sheehan.garrett23@gmail.com>
2021-07-25 10:30:14 -0500

    Click on Tiles working

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IClicked
 {
     // Start is called before the first frame update
 
     public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider;
     public bool isTraversable;
     public Piece pieceOnTile;
     public Vector2 tilePosition;
@@ -22,11 +23,14 @@ public class Tile : MonoBehaviour
         } else if(Instance != this){
             Destroy(gameObject);
         }
+
+        //Add and get sprite renderer
         gameObject.AddComponent<SpriteRenderer>();
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-     
-        
-               
+        gameObject.AddComponent<BoxCollider2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();  
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        boxCollider.size = new Vector2(1,1);                    
     }
 
     // Update is called once per frame
@@ -35,15 +39,23 @@ public class Tile : MonoBehaviour
         
     }
 
+    /**
+    Renders a sprite
+    */
     public void renderSprite(){
-        int tilePosSum = (int)(tilePosition.x + tilePosition.y);
-
-        Debug.Log(tilePosSum);
-
+        //Sums the x and y position of the board, varying visual element depending on if sum is even or odd
         if(((tilePosition.x + tilePosition.y) % 2) == 0){
             spriteRenderer.sprite = Resources.Load<Sprite>("blackTile");
         } else {
             spriteRenderer.sprite = Resources.Load<Sprite>("whiteTile");
         }
+    }
+
+    public void onClickTile(){
+        Debug.Log("Clicking tile");
+    }
+
+    public void onClickAction(){
+        Debug.Log("Tile " + tilePosition.x + "-" + tilePosition.y);
     }
 }
